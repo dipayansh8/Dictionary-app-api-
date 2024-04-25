274fa2ac73efb61703f1aa39fd50f9e406c5fbdc
dipayansh8 <dipayansh8@gmail.com>
2024-04-25 15:00:07 +0530

    2nd commit

--- script.js
+++ script.js
@@ -7,47 +7,45 @@ form.addEventListener("submit", (e) => {
 
 const getWordInfo = async (word) => {
     try {
-        resultDiv.innerHTML = "Fetching Data....."
-        // alert("word" + word);
+        resultDiv.innerHTML = "Fetchig data....";
         const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
         const data = await response.json();
         console.log(data);
         let definitions = data[0].meanings[0].definitions[0];
-        resultDiv.innerHTML = `
-    <h2><strong> Word:</strong>${data[0].word}</h2>
-    <p>${data[0].meanings[0].partofSpeech}</P>
-    <P><strong> Meaning:</strong>${definitions.definition === undefined ? "Not Found" : definitions.definition}</p>
-    <P><strong> Example:</strong>${definitions.example === undefined ? "Not Found" : definitions.example}</p>
-    <p><strong>Antonyms</strong></p>
-    
-    `;
+        resultDiv.innerHTML =
+            `<h2><strong>word:</strong>${data[0].word}</h2>
+<P><strong>pos:</strong>${data[0].meanings[0].partofSpeech}</P>
+<P><strong>Meaning:</strong>${definitions.definition === undefined ? "not found" : definitions.definition}</p>
+<P><strong>Example:</strong>${definitions.example === undefined ? "not found" : definitions.example}</p>
+<P><strong>Antonym:</strong></P>
+
+`
         // Fetching Antonym
         if (definitions.antonyms.length === 0)
-            resultDiv.innerHTML += `<span>Not Found</span>`;
+            resultDiv.innerHTML += `<span> Not Found</span > `;
         else {
             for (let i = 0; i < definitions.antonyms.length; i++) {
-                resultDiv.innerHTML += `<li>${definitions.antonyms[i]}</li>`
+                resultDiv.innerHTML += `< li > ${definitions.antonyms[i]}</li > `
             }
         }
-        resultDiv.innerHTML += `<p><strong>synonyms</strong></p>`;
+        // Fetching synonym
+        resultDiv.innerHTML += `<P> <strong>synonyms</strong></P > `;
         if (data[0].meanings[0].synonyms.length === 0)
-            resultDiv.innerHTML += `<span>Not Found</span>`;
+            resultDiv.innerHTML += `<span> Not Found</span > `;
         else {
-            for (let i = 0; i < data[0].meanings[0].synonyms.length; i++) {
-                resultDiv.innerHTML += `<li>${data[0].meanings[0].synonyms[i]}</li>`
-            }
+            for (i = 0; i < data[0].meanings[0].synonyms.length; i++)
+                resultDiv.innerHTML += `<li> ${data[0].meanings[0].synonyms}</li > `;
         }
 
 
         // Addin Read More button
-        resultDiv.innerHTML += `<div><a href =${data[0].sourceUrls}"target=_blank">Read More</a></div>`
+        resultDiv.innerHTML += `<p> <a href=${data[0].sourceUrls}><strong>Read More</strong></p>`;
     }
 
-    catch (error) {
-        resultDiv.innerHTML = `<p> Sorry , the word could not be found</P>`;
 
+    catch (error) {
+        resultDiv.innerHTML += '<P>sorry the word could not found</P>';
     }
 
 
 };
-// Task To Do -Fetch synonyms as we have done for antonym
